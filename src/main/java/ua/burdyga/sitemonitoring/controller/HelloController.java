f9dfc9033264e02e80ae1abe7b5c7457fe6ae93b
DimaSanKiev <dmytro.burdyga@gmail.com>
2015-06-18 22:52:04 +0300

    add web.xml and configure JSF

---
+++ src/main/java/ua/burdyga/sitemonitoring/controller/HelloController.java
@@ -0,0 +1,11 @@
+package ua.burdyga.sitemonitoring.controller;
+
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean(name = "helloController")
+public class HelloController {
+
+    public String showHello() {
+        return "hello from managed bean";
+    }
+}
