06b1d8eafc1e1eeab3b2293442a7c740157b3e55
DimaSanKiev <dmytro.burdyga@gmail.com>
2015-06-20 01:34:16 +0300

    add JSF validation with Hibernate Validator

--- src/main/java/ua/burdyga/sitemonitoring/entity/Check.java
+++ src/main/java/ua/burdyga/sitemonitoring/entity/Check.java
@@ -2,10 +2,12 @@ package ua.burdyga.sitemonitoring.entity;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.validator.constraints.URL;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.validation.constraints.Size;
 
 @Entity
 @Getter
@@ -16,7 +18,10 @@ public class Check {
     @GeneratedValue
     private int id;
 
+    @Size(min = 1, message = "Name cannot be empty!")
     private String name;
 
+    @Size(min = 1, message = "URL cannot be empty!")
+    @URL(message = "Invalid URL!")
     private String url;
 }
